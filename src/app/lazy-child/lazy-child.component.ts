bd99d988a9c4c0b4943bff03440101717c9af538
ross <ross.fleming@serviceware.se>
2019-07-01 16:14:13 +0200

    generate lazy module

---
+++ src/app/lazy-child/lazy-child.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy-child',
+  templateUrl: './lazy-child.component.html',
+  styleUrls: ['./lazy-child.component.css']
+})
+export class LazyChildComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
